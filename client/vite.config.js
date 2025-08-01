b488f991e143507b5f7f2950457df2f760a05bd6
iamdrishtiii <drishti.web.dev.work@gmail.com>
2025-08-02 01:50:11 +0530

    first commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
+   base: '/',
+    plugins: [react(),
+    tailwindcss()],
+    build: {
+      outDir: 'dist',
+    },
+    server: {
+      historyApiFallback: true, // important for dev mode
+    }
 })
